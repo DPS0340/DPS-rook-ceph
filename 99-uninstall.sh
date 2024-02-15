cdde57d1d5f733ccd7cc9f3c7e314494694efca3
Jiho Lee <optional.int@kakao.com>
2024-02-15 21:06:16 +0900

    feat: implement 99-uninstall.sh (without zap disk)

---
+++ 99-uninstall.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Enable debug output
+PS4="\n\033[1;33m>>\033[0m "; set -x
+
+LOCATION=$(realpath "$0")
+DIR=$(dirname "$LOCATION")
+
+sudo -v
+
+helm repo add rook https://charts.rook.io/release
+
+helm uninstall --create-namespace -n rook-ceph rook-ceph-cluster
+helm uninstall --create-namespace -n rook-ceph rook-ceph
