cf448bb60166c154a89d4d1bacd81e0b8bf1426d
Jiho Lee <optional.int@kakao.com>
2024-05-08 00:38:53 +0900

    feat: Delete related crs before cephcluster delete

--- _91-remove-cluster.sh
+++ _91-remove-cluster.sh
@@ -8,5 +8,10 @@ DIR=$(dirname "$LOCATION")
 
 sudo -v
 
+kubectl -n rook-ceph delete cephblockpool --all --wait
+kubectl -n rook-ceph delete cephfilesystem --all --wait
+kubectl -n rook-ceph delete cephfilesystemsubvolumegroup --all --wait
+kubectl -n rook-ceph delete cephobjectstore --all --wait
+
 kubectl -n rook-ceph patch cephcluster rook-ceph --type merge -p '{"spec":{"cleanupPolicy":{"confirmation":"yes-really-destroy-data"}}}'
 kubectl -n rook-ceph delete cephcluster rook-ceph --wait
